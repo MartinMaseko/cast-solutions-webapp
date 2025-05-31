3539e3a4346d31ae16c025b0a08ac5578b7d975a
martin_maseko <km.masekoccs@gmail.com>
2025-05-31 17:11:36 +0200

    Location.state removed & Import

--- src/components/Presentation.jsx
+++ src/components/Presentation.jsx
@@ -1,4 +1,4 @@
-import { useLocation, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { ref as dbRef, get } from "firebase/database";
 import { database } from "../firebaseConfig";
